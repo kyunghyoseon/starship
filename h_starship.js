b6e0be991f9f8a2c5e43410d18d990b3308c09e5
kyunghyoseon <hhyo_0@naver.com>
2023-09-27 01:18:12 +0900

    first

---
+++ h_starship.js
@@ -0,0 +1,20 @@
+﻿// $(function(){
+//페이지 로딩 시 제일 상단으로 스크롤 이동
+// $("body,html").stop().animate({"scrollTop":0},1500,"swing");		
+
+//모바일 메뉴
+// $(".k-hamburger").click(function(){
+// $("#k-menu").animate({marginLeft:"80%"},1000);
+// });
+// $("#k-menu >ul >li> a").click(function(){
+// if($(this).next().is(":visible")){
+// $(this).next().stop().slideUp(500);
+// }else{
+// $(".k-sub").stop().slideUp(500);
+// $(this).next().stop().slideDown(500);
+// };	
+// })
+// $(".k-cross").click(function(){
+// $("#k-menu").animate({marginLeft:"0%"},1000);	
+// });	
+// });
